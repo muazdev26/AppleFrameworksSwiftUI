3b5241dcdc0d540d2eb2dd09f8d427ea53721055
muazdev26 <funzoft.muaz@gmail.com>
2025-05-26 16:59:43 +0500

    Initial Commit

---
+++ AppleFrameworksSwiftUI/AppleFrameworksSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  AppleFrameworksSwiftUIApp.swift
+//  AppleFrameworksSwiftUI
+//
+//  Created by Apple on 26/05/2025.
+//
+
+import SwiftUI
+
+@main
+struct AppleFrameworksSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AppleFrameworksSwiftUI/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  AppleFrameworksSwiftUI
+//
+//  Created by Apple on 26/05/2025.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
